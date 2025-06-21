b79d2601ff99004ec8f9c103fb024a6aff430c07
fahmihwan <50146950+fahmihwan@users.noreply.github.com>
2025-06-22 04:01:56 +0700

    add laba

--- app/Http/Controllers/LabaController.php
+++ app/Http/Controllers/LabaController.php
@@ -40,39 +40,60 @@ class LabaController extends Controller
     public function detail($id)
     {
 
-
-
         $periode = Periode::find($id);
         if (!$periode) {
             return redirect('/periode')->with('error', 'Data tidak ditemukan');
         }
         $periodeKe = $periode->periode;
 
-        $pembelian = DB::select("SELECT 
-                            mt.type_tbs,
-                            COALESCE(SUM(pt.netto), 0) AS netto,
-                            COALESCE(pt.harga, 0) AS harga,
-                            COALESCE(SUM(pt.netto), 0) * COALESCE(pt.harga, 0) AS uang
-                        FROM m_type_tbs mt
-                        LEFT JOIN pembelian_tbs pt ON pt.tbs_type_id = mt.id
-                        LEFT JOIN periodes p ON pt.periode_id = p.id and p.periode = ?
-                        GROUP BY mt.type_tbs, COALESCE(pt.harga, 0);", [$periodeKe]);
 
-        $penjualan = DB::select("SELECT 
-                            dot.delivery_order_type,
-                            COALESCE(SUM(pj.netto), 0) AS netto,
-                            COALESCE(pj.harga, 0) AS harga,
-                            COALESCE(SUM(pj.netto), 0) * COALESCE(pj.harga, 0) AS uang
-                        FROM m_delivery_order_types dot
-                        LEFT JOIN penjualans pj ON pj.do_type_id = dot.id
-                        LEFT JOIN periodes p ON pj.periode_id = p.id and p.periode = ?
-                        GROUP BY dot.delivery_order_type, COALESCE(pj.harga, 0)
-                        ORDER BY dot.delivery_order_type;", [$periodeKe]);
+
+        $pembelian = collect(DB::select("WITH pembelian_periode AS (
+                                            SELECT 
+                                                pt.tbs_type_id,
+                                                pt.harga,
+                                                pt.netto,
+                                                p.periode
+                                            FROM pembelian_tbs pt
+                                            LEFT JOIN periodes p ON pt.periode_id = p.id
+                                            WHERE pt.deleted_at IS null and p.periode = ?
+                                        )
+
+                                        SELECT 
+                                            mt.type_tbs,
+                                            COALESCE(SUM(pp.netto), 0) AS netto,
+                                            COALESCE(MAX(pp.harga), 0) AS harga,
+                                            COALESCE(SUM(pp.netto), 0) * COALESCE(MAX(pp.harga), 0) AS uang
+                                        FROM m_type_tbs mt
+                                        LEFT JOIN pembelian_periode pp ON pp.tbs_type_id = mt.id AND pp.periode = ?
+                                        GROUP BY mt.type_tbs;
+                                        ", [$periodeKe, $periodeKe]));
+
+
+        $penjualan = collect(DB::select("WITH penjualan_periode AS (
+								SELECT 
+                                    pj.do_type_id,
+                                    pj.netto,
+                                    pj.harga,
+                                    p.periode
+                                from penjualans pj 
+                                LEFT JOIN periodes p ON pj.periode_id = p.id 
+                                WHERE p.deleted_at IS null and p.periode = ?
+                            )
+                           	SELECT 
+							    dot.delivery_order_type,
+                                COALESCE(SUM(pj.netto), 0) AS netto,
+                                COALESCE(max(pj.harga), 0) AS harga,
+                                COALESCE(SUM(pj.netto), 0) * COALESCE(max(pj.harga), 0) AS uang
+							FROM m_delivery_order_types dot
+							LEFT JOIN penjualan_periode pj on pj.do_type_id = dot.id AND pj.periode = ?
+							GROUP BY dot.delivery_order_type", [$periodeKe, $periodeKe]));
 
 
         return view('pages.laba.detail', [
             'pembelian' => $pembelian,
-            'penjualan' => $penjualan
+            'penjualan' => $penjualan,
+            'periode' => $periode
         ]);
     }
 }
--- resources/views/pages/laba/detail.blade.php
+++ resources/views/pages/laba/detail.blade.php
@@ -17,8 +17,27 @@
 @section('container')
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header">Laba</h1>
+            <div class="col-md-12">
+                <div style="display: flex; justify-content: space-between;  align-items: center">
+                    <div style="display: flex; align-items: center">
+                        <h1 class="page-header">Laba periode ke-{{ $periode->periode }}
+                        </h1>
+                        <h3>
+                            @if (!$periode->periode_berakhir)
+                                <span style="color:red; margin-left: 20px">(Belum ditutup)</span>
+                            @endif
+                        </h3>
+
+                    </div>
+                    <h3>
+                        <b>{{ \Carbon\Carbon::parse($periode->periode_mulai)->format('d M Y') }}</b>
+                        @if ($periode->periode_berakhir)
+                            sampai
+                            <b>{{ \Carbon\Carbon::parse($periode->periode_berakhir)->format('d M Y') }}</b>
+                        @endif
+                    </h3>
+
+                </div>
             </div>
             <!-- /.col-lg-12 -->
         </div>
@@ -36,62 +55,17 @@
             @endif
         </div>
         <div class="row">
-            <div class="col-lg-12">
+            <div class="col-md-12">
                 <div class="panel panel-default">
                     <div class="panel-heading" style="display: flex; justify-content: space-between">
                         <div>
                             Pembelian
                         </div>
-
-
-                        {{-- 
-                        <form method="GET" action="/slipgaji/karyawan/{{ $karyawan->id }}" style="display: flex">
-                            <div class="" style="margin-right: 10px">
-                                <select name="bulan" class="form-control">
-                                    <option value="">Pilih Bulan</option>
-                                    @for ($i = 1; $i <= 12; $i++)
-                                        @php $val = str_pad($i, 2, '0', STR_PAD_LEFT); @endphp
-                                        <option value="{{ $val }}" {{ $selectedBulan == $val ? 'selected' : '' }}>
-                                            {{ \Carbon\Carbon::create()->month($i)->translatedFormat('F') }}
-                                        </option>
-                                    @endfor
-                                </select>
-                            </div>
-
-                            <div class="">
-                                <select name="tahun" class="form-control">
-                                    <option value="">Pilih Tahun</option>
-                                    @for ($y = now()->year; $y >= 2024; $y--)
-                                        <option value="{{ $y }}" {{ $selectedTahun == $y ? 'selected' : '' }}>
-                                            {{ $y }}
-                                        </option>
-                                    @endfor
-                                </select>
-                            </div>
-
-                            <div class="form-filter-datatables">
-                                <button type="submit" class="btn btn-primary btn-sm">
-                                    <i class="fa fa-search"></i> Search
-                                </button>
-                            </div>
-
-                            <div class="form-filter-datatables">
-                                <a href="/slipgaji/karyawan/{{ $karyawan->id }}?bulan={{ now()->format('m') }}&tahun={{ now()->format('Y') }}"
-                                    class="btn btn-info btn-sm">
-                                    <i class="fa fa-refresh"></i> clear
-                                </a>
-                            </div>
-
-
-                        </form> --}}
-
-
-                        {{--  --}}
                     </div>
                     <!-- /.panel-heading -->
                     <div class="panel-body">
                         <div class="table-responsive">
-                            <table class="table table-striped table-bordered table-hover">
+                            <table class="table  table-bordered ">
                                 <thead>
                                     <tr style="text-align: center">
                                         <th style="text-align: center">Jenis TBS</th>
@@ -101,28 +75,135 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <td>TBS RAM</td>
-
-                                    </tr>
-                                    <tr>
-                                        <td>TBS LAHAN</td>
-                                    </tr>
-                                    <tr>
-                                        <td>TBS RUMAH</td>
-                                    </tr>
-                                    {{-- @foreach ($items as $item)
+                                    @foreach ($pembelian as $item)
                                         <tr>
-                                            <td>{{ $loop->iteration }}</td>
-                                            <td style="{{ $item['alpha'] ? 'background-color: red; color:white' : '' }}">
-                                                {{ $item['created_at_formatted'] }}</td>
-                                            <td>{{ $item['netto'] }} kg</td>
-                                            <td>{{ $item['tarif_perkg_rp'] }}</td>
-                                            @for ($i = 0; $i < $colspanTKBM; $i++)
-                                                <td>{{ isset($item['tkbms'][$i]) ? $item['tkbms'][$i] : '-' }}</td>
-                                            @endfor
-                                            <td>{{ $item['total'] }}</td>
-                                            <td>{{ $item['jumlah_uang_rp'] }}</td>
+                                            <td>{{ $item->type_tbs }}</td>
+                                            <td>{{ number_format($item->netto, 0, ',', '.') }} Kg</td>
+                                            <td>Rp {{ number_format($item->harga, 0, ',', '.') }}</td>
+                                            <td>Rp {{ number_format($item->uang, 0, ',', '.') }}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                                <tfoot>
+                                    <th>
+                                        Total
+                                    </th>
+                                    <th style="background-color: yellow">
+                                        {{ number_format($pembelian->sum('netto'), 0, ',', '.') }} Kg
+                                    </th>
+                                    <th>
+
+                                    </th>
+                                    <th style="background-color: yellow">
+                                        Rp {{ number_format($pembelian->sum('uang'), 0, ',', '.') }}
+                                    </th>
+                                </tfoot>
+                            </table>
+                        </div>
+                        <!-- /.table-responsive -->
+                    </div>
+                    <!-- /.panel-body -->
+                </div>
+                <!-- /.panel -->
+            </div>
+            <!-- /.col-lg-12 -->
+        </div>
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading" style="display: flex; justify-content: space-between">
+                        <div>
+                            Penjualan
+                        </div>
+                    </div>
+                    <!-- /.panel-heading -->
+                    <div class="panel-body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered ">
+                                <thead>
+                                    <tr style="text-align: center">
+                                        <th style="text-align: center">Tonase Pabrik</th>
+                                        <th style="text-align: center">Harga</th>
+                                        <th style="text-align: center">Uang</th>
+                                        <th style="text-align: center">Jenis DO</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($penjualan as $item)
+                                        <tr>
+
+
+                                            <td>{{ number_format($item->netto, 0, ',', '.') }} Kg</td>
+                                            <td>Rp {{ number_format($item->harga, 0, ',', '.') }}</td>
+                                            <td>Rp {{ number_format($item->uang, 0, ',', '.') }}</td>
+                                            <td>{{ $item->delivery_order_type }}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                                <tfoot>
+                                    <th style="background-color: yellow">
+                                        {{ number_format($penjualan->sum('netto'), 0, ',', '.') }} Kg
+                                    </th>
+                                    <th>
+
+                                    </th>
+                                    <th style="background-color: yellow">
+                                        Rp {{ number_format($penjualan->sum('uang'), 0, ',', '.') }}
+                                    </th>
+                                    <th>
+
+                                    </th>
+                                </tfoot>
+                            </table>
+                        </div>
+                        <!-- /.table-responsive -->
+                    </div>
+                    <!-- /.panel-body -->
+                </div>
+                <!-- /.panel -->
+            </div>
+            <!-- /.col-lg-12 -->
+        </div>
+
+        <div class="row">
+            <div class="col-md-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading" style="display: flex; justify-content: space-between">
+                        <div>
+                            Perhitungan Laba
+                        </div>
+                    </div>
+                    <!-- /.panel-heading -->
+                    <div class="panel-body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered ">
+                                <tbody>
+                                    <tr>
+                                        <th>Cair Do</th>
+                                        <td>Rp {{ number_format($penjualan->sum('uang'), 0, ',', '.') }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Modal</th>
+                                        <td>Rp {{ number_format($pembelian->sum('uang'), 0, ',', '.') }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>OPS</th>
+                                        <td>Rp {{ number_format($penjualan->sum('netto') * 120, 0, ',', '.') }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Laba Bersih</th>
+                                        <td style="background-color: yellow">Rp
+                                            {{ number_format($penjualan->sum('uang') - $pembelian->sum('uang') - $penjualan->sum('netto') * 120, 0, ',', '.') }}
+                                        </td>
+                                    </tr>
+                                    {{-- @foreach ($penjualan as $item)
+                                        <tr>
+
+
+                                            <td>{{ number_format($item->netto, 0, ',', '.') }} Kg</td>
+                                            <td>Rp {{ number_format($item->harga, 0, ',', '.') }}</td>
+
                                         </tr>
                                     @endforeach --}}
                                 </tbody>
